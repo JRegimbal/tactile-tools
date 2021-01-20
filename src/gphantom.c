5a252685be32983a91ab5cda61e17c6615251f27
Juliette Regimbal <juliette.regimbal@gmail.com>
2021-01-20 09:34:01 -0500

    Working version of gphantom

--- src/gphantom.c
+++ src/gphantom.c
@@ -6,6 +6,7 @@
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
+const t_float EPSILON = 0.5;
 static t_class *gphantom_class;
 
 typedef struct _gphantom {
@@ -15,8 +16,8 @@ typedef struct _gphantom {
 } t_gphantom;
 
 void gphantom_set(t_gphantom *x, t_floatarg f1, t_floatarg f2, t_floatarg f3) {
-    x->f_x = MIN(x->i_m, MAX(0, f1));
-    x->f_y = MIN(x->i_n, MAX(0, f2));
+    x->f_x = MIN(x->i_m - 1, MAX(0, f1));
+    x->f_y = MIN(x->i_n - 1, MAX(0, f2));
     x->f_a = MAX(0, f3);
 }
 
@@ -26,10 +27,24 @@ void gphantom_reset(t_gphantom *x) {
     x->f_a = 0;
 }
 
+t_float gaussian(t_float epsilon, t_float r) {
+    return (t_float) exp(-pow(epsilon * r, 2));
+}
+
 void gphantom_bang(t_gphantom *x) {
-   // TODO do computation
-   // make a list
-   // outlet_list(x->, &s_list, int argc, t_atom *argv)
+   int argc = x->i_n * x->i_m;
+   t_atom *argv = (t_atom *)getbytes(sizeof(t_atom) * argc);
+
+   for (int i = 0; i < x->i_m; i++) {   // x coord
+       for (int j = 0; j < x->i_n; j++) {    // y coord
+           t_float distance = (float)sqrt(pow((float)i - x->f_x, 2) + pow((float)j - x->f_y, 2));
+           t_float amp = x->f_a * gaussian(EPSILON, distance);
+           size_t index = (i * x->i_n) + j;
+           SETFLOAT(argv + index, amp);
+       }
+   }
+
+   outlet_list(x->x_obj.ob_outlet, &s_list, argc, argv);
 }
 
 void gphantom_list(t_gphantom *x, t_symbol *s, t_int argc, t_atom *argv) {
@@ -39,7 +54,7 @@ void gphantom_list(t_gphantom *x, t_symbol *s, t_int argc, t_atom *argv) {
             gphantom_bang(x);
             break;
         default:
-            error("[gphantom ]: three arguments are expected (x, y, amp)!");
+            error("[gphantom ]: three arguments are expected (x, y, amp); received %ld!", argc);
             break;
     }
 }
@@ -49,9 +64,10 @@ void *gphantom_new(t_floatarg n, t_floatarg m) {
 
     x->i_n = n;
     x->i_m = m;
-    post("Created with %d, %d", n, m);
     gphantom_reset(x);
     outlet_new(&x->x_obj, &s_list);
+
+    post("[gphantom ]: created instance with %ld by %ld grid", x->i_n, x->i_m);
 
     return (void *)x;
 }
